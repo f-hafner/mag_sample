9f4e56f5785644c8a37cf161bb7badf93d000bd6
Christoph Hedtrich <christophhedtrich@gmail.com>
2022-09-06 17:35:27 +0200

    update advisor linking scripts for next run

--- src/dataprep/main/link/link_onefield_advisors.sh
+++ src/dataprep/main/link/link_onefield_advisors.sh
@@ -9,6 +9,7 @@ fieldofstudy_cat=$5
 fieldofstudy_str=$6
 keywords=$7
 logfile_path=$8
+
 echo "$field"
 
 mergemode="m:1"
@@ -18,8 +19,8 @@ python3 -m main.link.train_link_mag_proquest --linking_type "advisors" --no-test
     --fieldofstudy_cat $fieldofstudy_cat --fieldofstudy_str $fieldofstudy_str --keywords $keywords \
     --verbose 2>&1 | tee $logfile_path/trainlink_mag_proquest_"${field}"_${train_name}_advisors_8522.log 
 
-python3 -m main.link.create_link_mag_proquest --linking_type "advisors" --no-test --mergemode $mergemode --train_name $train_name \
-    --field "${field}" --recall $RECALL --start 1985 --end 2022 --institution $institution \
-    --fieldofstudy_cat $fieldofstudy_cat --fieldofstudy_str $fieldofstudy_str --keywords $keywords \
-    --verbose 2>&1 | tee $logfile_path/createlink_mag_proquest_"${field}"_${mergemode}_${train_name}_advisors_8522.log 
+# python3 -m main.link.create_link_mag_proquest --linking_type "advisors" --no-test --mergemode $mergemode --train_name $train_name \
+#     --field "${field}" --recall $RECALL --start 1985 --end 2022 --institution $institution \
+#     --fieldofstudy_cat $fieldofstudy_cat --fieldofstudy_str $fieldofstudy_str --keywords $keywords \
+#     --verbose 2>&1 | tee $logfile_path/createlink_mag_proquest_"${field}"_${mergemode}_${train_name}_advisors_8522.log 
  
--- src/dataprep/pipeline_advisors.sh
+++ src/dataprep/pipeline_advisors.sh
@@ -6,5 +6,29 @@ output_path="../../../output/"
 script_path="main"
 logfile_path="temp/"
 
-echo "Starting main script"
-bash $script_path/link/advisors.sh $logfile_path
+## for conda 
+BASEENV='base'
+NECESSARYENV='science-career-tempenv'
+NECESSARYVERSION="2.0.11"
+
+# ## 2. Activate the right conda environment for deduping
+eval "$(conda shell.bash hook)"
+
+ENV=$(conda info | grep "active environment" | awk '{ gsub("\t active environment : ", ""); print}')
+if ! grep -q "$BASEENV" <<< "$ENV"; then
+  echo "Base environment not activated."
+  exit 
+fi
+
+DEDUPEVERSION=$(conda list -n science-career-tempenv | grep 'dedupe ')
+if ! grep -q "$NECESSARYVERSION" <<< $DEDUPEVERSION; then 
+  echo "Wrong version of dedupe in environment science-career-tempenv."
+  conda deactivate
+else
+    conda activate $NECESSARYENV
+    # ## 2. Link NSF grants to MAG advisors
+    echo "Starting main script"
+    bash $script_path/link/advisors.sh $logfile_path
+    # ## 3. Finish
+    conda deactivate
+fi 
