442ebdc82e1e0ac20434cc24496a61d51ee3e602
Christoph Hedtrich <christophhedtrich@gmail.com>
2022-09-07 17:15:20 +0000

    redo commit with branched based on main

---
+++ src/dataprep/main/institutions/distances_cng.py
@@ -0,0 +1,36 @@
+# run as python -m src.dataprep.main.institutions.distances_cng
+# Calculate distance between cng institutions
+
+import sqlite3 as sqlite
+import pandas as pd
+import numpy as np 
+from geopy import distance
+import os
+
+print(os.getcwd())
+
+from src.dataprep.helpers.variables import db_file
+import src.dataprep.main.institutions.sql_queries as sq
+
+import matplotlib.pyplot as plt
+
+
+con = sqlite.connect(db_file)
+
+with con:
+    cng = pd.read_sql(sql=sq.query_cng, con=con)
+
+con.close()
+
+cng = cng[["unitid", "lat", "lon"]]
+cng_rhs = cng.copy(deep=True)
+cng_rhs.rename(columns={"unitid":"unitid2", "lat":"lat2", "lon":"lon2"}, inplace=True)
+cng = cng.merge(cng_rhs, how="cross")
+
+print(cng.head())
+
+# calculate distance
+cng['distance_km'] = cng.apply(lambda row: distance.distance((row['lat'], row['lon']), (row['lat2'], row['lon2'])).km, axis=1)
+print(cng.head())
+
+cng[['unitid','unitid2','distance']].to_csv("./data/institutions/calculated_distance_cng.csv")
