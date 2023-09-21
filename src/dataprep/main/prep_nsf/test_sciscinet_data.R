3631652d722e6f74004540e222cce6c3f5d637aa
mona <m.dapfer@outlook.de>
2023-09-21 16:45:49 +0000

    Download SciSciNet_NSF and link to mag authors

---
+++ src/dataprep/main/prep_nsf/test_sciscinet_data.R
@@ -0,0 +1,44 @@
+# Link SciSciNet_Links_NSF table with Paper_Author_Affiliations, Authors, and NSF_Investigator 
+# Keeps only those with link between NSF grant and author ID.
+# Data downloaded and uploaded into db in: scinet_data_to_db.py in same folder
+
+packages <- c("tidyverse", "broom", "dbplyr", "RSQLite", "ggplot2", "stringdist", "dyplyr", "DBI")
+lapply(packages, library, character.only = TRUE)
+
+datapath <- "/mnt/ssd/"
+db_file  <- paste0(datapath, "AcademicGraph/AcademicGraph.sqlite")
+sciscinet_path <- paste0(datapath,"sciscinet_data/")
+
+
+filepath_nsf=paste0(sciscinet_path,"SciSciNet_Link_NSF.tsv")
+
+con <- DBI::dbConnect(RSQLite::SQLite(), db_file)
+cat("The database connection is: \n")
+src_dbi(con)
+
+# Create table with all links between NSF-grant and authors via papers 
+
+NSF_to_Authors <- tbl(con, sql("
+                  select a. PaperID, a.Type, a.GrantID, b.AuthorId, b.OriginalAuthor 
+                        ,c.NormalizedName, Position, FirstName, LastName
+                                      from scinet as a
+                                      inner join (
+                                        select PaperId AS PaperID, AuthorId, OriginalAuthor
+                                        from PaperAuthorAffiliations 
+                                      )b 
+                                      using (PaperID)
+                                      inner join (
+                                        select AuthorId, NormalizedName
+                                        from Authors
+                                      ) c
+                                      using (AuthorId)
+                                      inner join (
+                                        select GrantID, Position, FirstName, LastName
+                                        from NSF_Investigator
+                               ) d 
+                               using (GrantID)
+                               "))
+
+nsf_to_authors <- collect(NSF_to_Authors)
+
+DBI::dbDisconnect(con)
