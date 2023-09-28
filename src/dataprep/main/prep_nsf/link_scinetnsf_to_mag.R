47aa945b5a5bbf8c8d2e95815da080c3f75936bd
mona <m.dapfer@outlook.de>
2023-09-28 12:58:53 +0000

    Added info on processing stage + updated pipeline

--- src/dataprep/main/prep_nsf/link_scinetnsf_to_mag.R
+++ src/dataprep/main/prep_nsf/link_scinetnsf_to_mag.R
@@ -3,6 +3,11 @@
 # only those links with a similar name (similarity >=0.8) are loaded into db
 # Data downloaded and uploaded into db in: scinet_data_to_db.py in same folder
 
+# Initialize variables for counting rows and timestamp
+row_count <- 0
+start_time <- Sys.time()
+cat(sprintf("Started at", start_time))
+
 packages <- c("tidyverse", "broom", "dbplyr", "RSQLite", "stringdist")
 lapply(packages, library, character.only = TRUE)
 
@@ -13,6 +18,7 @@ db_file  <- paste0(datapath, "AcademicGraph/AcademicGraph.sqlite")
 con <- DBI::dbConnect(RSQLite::SQLite(), db_file)
 cat("The database connection is: \n")
 src_dbi(con)
+cat("Connected to db...\n")
 
 # Create table with all links between NSF-grant and authors via papers
 
@@ -73,6 +79,7 @@ firstname_similarity <- numeric(0)
 lastname_similarity <- numeric(0)
 
 # Iterate through rows and calculate string distances for first and last names separately
+cat("Start comparing names...\n")
 for (i in 1:nrow(nsf_to_authors)) {
   mag_firstname <- nsf_to_authors$mag_firstname[i]
   nsf_firstname <- nsf_to_authors$nsf_firstname[i]
@@ -83,8 +90,7 @@ for (i in 1:nrow(nsf_to_authors)) {
     nsf_firstname,
     method="osa" )
   
-  # Append the calculated distance to the results vector for first name
- firstname_similarity <- c(firstname_similarity, first_row_similarity)
+
 
  # Calculate string distance for last name by row
     mag_lastname <- nsf_to_authors$mag_lastname[i]
@@ -96,9 +102,41 @@ for (i in 1:nrow(nsf_to_authors)) {
      method="osa"
   )
   
-  # Append the calculated distance to the results vector for last name
+  # Append the calculated distances to the results vector 
+  firstname_similarity <- c(firstname_similarity, first_row_similarity)
   lastname_similarity <- c(lastname_similarity, last_row_similarity)
+
+  # Increment row count
+  row_count <- row_count + 1
+  
+  # Progress after each 500,000th row
+  if (row_count %% 50 == 0) {
+    # Calculate elapsed time
+    elapsed_time <- Sys.time() - start_time 
+    elapsed_time <- as.numeric(elapsed_time)
+    
+    # Calculate percentage of data processed
+    percent_processed <- (row_count / nrow(nsf_to_authors)) * 100
+    
+    # Some information
+    cat(sprintf(
+      "Processed %d rows (%.2f%%) in %2.f minutes.\n",
+      row_count, 
+      percent_processed, 
+      elapsed_time
+    ))
+  }
 }
+
+elapsed_time <- Sys.time() - start_time
+elapsed_time <- as.numeric(elapsed_time)
+
+percent_processed <- (row_count / nrow(nsf_to_authors)) * 100
+cat(sprintf(
+  "Processed all rows (%.2f%%) in %2.f minutes.\n",
+  percent_processed, 
+  elapsed_time
+))
 
 # Assign the calculated distances to a new column in data frame
 nsf_to_authors$firstname_similarity <- firstname_similarity
@@ -114,7 +152,19 @@ df <- similar_names %>%
   distinct()
 
 # Write table to db:
-dbWriteTable(con, name = "links_nsf_mag", value = df, overwrite = TRUE)
+cat("Starting data upload to the database...\n")
+#dbWriteTable(con, name = "links_nsf_mag2", value = df, overwrite = TRUE)
+cat("Data upload to the database is complete.\n")
+
+# Some info
+final_elapsed_time <- Sys.time() - start_time
+elapsed_time <- as.numeric(elapsed_time)
+
+cat(sprintf(
+  "Complete. Total time elapsed: %2.f minutes.\n",
+  elapsed_time
+))
 
 # close connection to db
 DBI::dbDisconnect(con)
+cat("Disconnected from db.\n")
