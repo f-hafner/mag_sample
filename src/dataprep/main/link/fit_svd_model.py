9c9edc2cb51f2bc827982ebf4f97ea9f2adfaf05
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-10-02 15:56:22 +0200

    refactor script for svd model

--- src/dataprep/main/link/fit_svd_model.py
+++ src/dataprep/main/link/fit_svd_model.py
@@ -1,48 +1,53 @@
 """Prepare data and fit truncated SVD on subsample of papers"""
 
 
-# import ibis
-# from ibis import _
-import matplotlib.pyplot as plt  
 import numpy as np 
 import sqlite3 as sqlite
 import numpy as np      
 import pandas as pd 
 from pickle import dump
+import logging
 
 from scipy.sparse import csr_matrix                                                                                                                                                         
 from sklearn.decomposition import TruncatedSVD  
-import scipy
 from helpers.variables import db_file, insert_questionmark_doctypes, keep_doctypes
 
 
-random_seed = 583523592352
-sample_size = 1_000_000
+# TODO: valid_papers: should be persistent? -> for running the model later on
+
+logging.basicConfig(level=logging.INFO)
+
+
 N_DIM = 1024
+SAMPLE_SIZE = 1_000_000
+DRY_RUN = True
+MODEL_URL = "/mnt/ssd/AcademicGraph/svd_model"
+
+
+RANDOM_SEED = 583523592352
+sample_size = 1_000_000
 
 # TODO: is this correct? -- doublecheck with Christoph
-DRY_RUN = True
-SAMPLE_SIZE = 1_000_000
 max_level = 2
 start_year = 1980
 end_year = 2020
 
-MODEL_URL = "/mnt/ssd/AcademicGraph/svd_model"
 
 
 def make_sparse(long_df, field_to_index, rows="AffiliationId", cols="FieldOfStudyId", value_col="score"): 
-    # col = concepts_affiliations['AffiliationId']
-    # row = 
     """Create sparse matrix from a dataframe.
 
     Args:
-        col (np.ndarray): values for index 0 in the resulting sparse matrix
-        row (np.ndarray): values for index 1 in the resulting sparse matrix.
-        row_map: mapping of all possible row values to identifiers.
-        data: non-zero values to to fill the sparse matrix.  
+        long_df (pd.DataFrame): the data to be converted.
+        field_to_index (dict): exhaustive mapping of field of study id to column 
+        index in the resulting matrix.
+        rows (str): column name in `long_df` to be converted to rows.
+        cols (str): column name in `long_df` to be converted to columns.
+        value_col (str): column name in `long_df` for non-zero elements in the 
+        sparse matrix.
 
     Returns:
-    (sparse matrix, map from row IDs in dataframe to row index in the sparse matrix)
+        tuple: (sparse matrix, map from row IDs in dataframe to row index in the sparse matrix)
     """ 
 
     row_values = np.array(long_df[rows])
@@ -74,7 +79,7 @@ def run_svd(in_matrix, n_components=50):
         n_components: dimension of the reduced feature space.
 
     Returns:
-    (svd model, embeddings in subspace)
+        tuple: (svd model, embeddings in subspace)
     """
     svd = TruncatedSVD(n_components=n_components, random_state=42) 
     embs = svd.fit_transform(in_matrix)
@@ -87,96 +92,124 @@ def run_svd(in_matrix, n_components=50):
 
 
 
-generator = np.random.default_rng(random_seed)
-sqlite.register_adapter(np.int64, lambda val: int(val))
-con = sqlite.connect(database = db_file, isolation_level= None)
+def prepare_tables(con):
+    "Prepare temp tables for loading papers and fields"
+    logging.info("making fields_to_max_level")
+    con.execute(
+            """CREATE TEMP TABLE fields_to_max_level AS
+            SELECT FieldOfStudyId
+            FROM FieldsOfStudy
+            WHERE Level > 0 AND Level <= (?)
+            """
+            , (max_level,)
+            )
+    con.execute("CREATE UNIQUE INDEX idx_temp1 ON fields_to_max_level(FieldOfStudyId ASC)")
+
+    logging.info("creating valid papers")
+
+    con.execute("DROP TABLE IF EXISTS valid_papers")
+    sql_valid_papers = f"""CREATE TABLE valid_papers AS
+        SELECT PaperId
+        FROM Papers
+        WHERE Year >= (?)
+        AND Year <= (?)
+        AND DocType IN ({insert_questionmark_doctypes})"""
+    
+    if DRY_RUN:
+        sql_valid_papers += " LIMIT 1000"
 
 
+    logging.debug(sql_valid_papers)
+
+    con.execute(sql_valid_papers, (start_year, end_year, *keep_doctypes))
 
 
-print("making fields_to_max_level", flush=True)
-con.execute(
-    """CREATE TEMP TABLE fields_to_max_level AS
-    SELECT FieldOfStudyId
-    FROM FieldsOfStudy
-    WHERE Level > 0 AND Level <= (?)
+def sample_papers(con):
+    "Sample papers and write to database"
+    
+    generator = np.random.default_rng(RANDOM_SEED)
+    
+    all_papers = pd.read_sql("SELECT PaperId FROM valid_papers", con=con) 
+
+    if DRY_RUN:
+        subsample = all_papers.sample(n=500, random_state=generator)
+    else:
+        subsample = all_papers.sample(n=SAMPLE_SIZE, random_state=generator)
+
+    logging.info("writing sampled papers")
+
+    cursor = con.cursor()
+    data_to_insert = [(x,) for x in subsample["PaperId"].values]
+    # sql_delete = "DROP TABLE IF EXISTS selected_papers_svd"
+    sql_create = "CREATE TEMP TABLE selected_papers_svd (PaperId INT)"
+
+    cursor.execute(sql_create)
+    cursor.executemany("INSERT INTO selected_papers_svd (PaperId) VALUES (?)", data_to_insert)
+    cursor.execute("CREATE UNIQUE INDEX idx_paperid_selp ON selected_papers_svd(PaperId)")
+    con.commit()
+
+
+def load_data(con):
+    """Load data for SVD decomposition
+        
+    Returns:
+        tuple: (dataframe with papers, dataframe with exhaustive FieldOfStudyIds) 
     """
-    , (max_level,)
-)
-con.execute("CREATE UNIQUE INDEX idx_temp1 ON fields_to_max_level(FieldOfStudyId ASC)")
+    
+    sql_load_papers = """
+        SELECT PaperId, FieldOfStudyId, Score
+        FROM valid_papers
+        INNER JOIN PaperFieldsOfStudy
+        USING (PaperId)
+        INNER JOIN fields_to_max_level
+        USING (FieldOfStudyId)
+        INNER JOIN selected_papers_svd
+        USING (PaperId)
+    """
 
-print("creating valid papers", flush=True)
-
-sql_valid_papers = f"""CREATE TEMP TABLE valid_papers AS
-    SELECT PaperId
-    FROM Papers
-    WHERE Year >= (?)
-    AND Year <= (?)
-    AND DocType IN ({insert_questionmark_doctypes})"""
-if DRY_RUN:
-    sql_valid_papers += " LIMIT 1000"
-
-print(sql_valid_papers)
-
-con.execute(
-    sql_valid_papers, (start_year, end_year, *keep_doctypes)
-)
+    papers_fields = pd.read_sql(sql_load_papers, con=con) 
+    fields_of_study = pd.read_sql("select * from fields_to_max_level", con=con)
+    
+    return papers_fields, fields_of_study
 
 
-print("loading valid papers", flush=True)
-all_papers = pd.read_sql("SELECT * FROM valid_papers", con=con) 
+def main():
+    
+    model_url = MODEL_URL
+    sqlite.register_adapter(np.int64, lambda val: int(val))
+    con = sqlite.connect(database = db_file, isolation_level= None)
 
 
-if DRY_RUN:
-    subsample = all_papers.sample(n=500, random_state=generator)
-else:
-    subsample = all_papers.sample(n=SAMPLE_SIZE, random_state=generator)
+    prepare_tables(con)
 
-print("writing sampled papers", flush=True)
+    logging.info("loading valid papers")
+    sample_papers(con) 
+    
+    
+    logging.info("loading sampled papers and fields")
+    papers_fields, fields_of_study = load_data(con) 
+    
+    field_to_index = {id: index for index, id in enumerate(fields_of_study['FieldOfStudyId'].unique())}
+   
+    logging.info("making sparse matrix and running SVD")
+    papers_fields_sparse, _ = make_sparse(
+        papers_fields, field_to_index, "PaperId", "FieldOfStudyId", "Score")
+    
+    svd, _  = run_svd(papers_fields_sparse, N_DIM)
+    
+   
+    logging.info("saving model")
 
-cursor = con.cursor()
-data_to_insert = [(x,) for x in subsample["PaperId"].values]
-# sql_delete = "DROP TABLE IF EXISTS selected_papers_svd"
-sql_create = "CREATE TEMP TABLE selected_papers_svd (PaperId INT)"
-
-cursor.execute(sql_create)
-cursor.executemany("INSERT INTO selected_papers_svd (PaperId) VALUES (?)", data_to_insert)
-cursor.execute("CREATE UNIQUE INDEX idx_paperid_selp ON selected_papers_svd(PaperId)")
-con.commit()
-
-
-print("loading sampled papers and fields", flush=True)
-sql_load_papers = """
-    SELECT PaperId, FieldOfStudyId, Score
-    FROM valid_papers
-    INNER JOIN PaperFieldsOfStudy
-    USING (PaperId)
-    INNER JOIN fields_to_max_level
-    USING (FieldOfStudyId)
-    INNER JOIN selected_papers_svd
-    USING (PaperId)
-"""
-
-papers_fields = pd.read_sql(sql_load_papers, con=con) 
-fields_of_study = pd.read_sql("select * from fields_to_max_level", con=con)
-
-field_to_index = {id: index for index, id in enumerate(fields_of_study['FieldOfStudyId'].unique())}
-field_to_index_map = np.vectorize(field_to_index.get)
+    if DRY_RUN:
+        model_url += "_dry"
+    
+    with open(model_url + ".pkl", "wb") as f:
+        dump(svd, f, protocol=5)
+    
+    logging.info("Done.")
 
 
-papers_fields_sparse, row_to_index = make_sparse(
-    papers_fields, field_to_index, "PaperId", "FieldOfStudyId", "Score")
-
-svd, embs = run_svd(papers_fields_sparse, N_DIM)
-
-
-if DRY_RUN:
-    MODEL_URL += "_dry"
-
-with open(MODEL_URL + ".pkl", "wb") as f:
-    dump(svd, f, protocol=5)
-
-print("Done.")
-
+if __name__ == "__main__":
+    main()
 
 
