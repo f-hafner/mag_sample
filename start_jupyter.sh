a4eee7c9438d3b8261430c4f2a01c2600b6df14a
Flavio Hafner <hafner.flavio@gmail.com>
2024-10-20 17:14:22 +0200

    fix start_jupyter

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,8 +1,14 @@
 
-# Start a jupyter lab server. Call with `source start_jupyter.sh`
+# Start a jupyter lab server on remote.
+# 1. Call with `source start_jupyter.sh`
+# 2. On local machine, use port forwarding as described here: https://vmascagn.web.cern.ch/vmascagn/LABO_2020/jupyter_remote.html
+# 3. Click on the link with localhost in it, and jupyter lab should start.
 
 eval "$(conda shell.bash hook)"
 conda activate science-career-tempenv
 
-env PYTHONPATH="$pwd"/src/dataprep/ jupyter-lab --no-browser
+repo_dir=$(pwd)
+echo "$repo_dir"/src/dataprep
+env PYTHONPATH="$repo_dir"/src/dataprep/ jupyter-lab --no-browser
+
 
